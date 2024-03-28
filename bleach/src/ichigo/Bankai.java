02712d3ed59a153ddd8386c38dd58d641ecc8e2c
vicky <vicky@vignesh_B>
2024-03-28 16:30:26 +0530

    "basic"

---
+++ bleach/src/ichigo/Bankai.java
@@ -0,0 +1,19 @@
+package ichigo;
+
+public class Bankai {
+	public void studentname() {
+		System.out.println("Vicky");
+	}
+	public void studentage() {
+		System.out.println("18");
+	}
+
+	public static void main(String[] args) {
+		Bankai obj = new Bankai();
+		obj.studentname();
+		obj.studentage();                                                       
+		
+
+	}
+
+}
