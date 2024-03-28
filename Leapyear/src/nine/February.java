cb98a6a6e2335b67eb6fc79bb5da69f16823cb40
vicky <vicky@vignesh_B>
2024-03-28 16:39:46 +0530

    "new"

---
+++ Leapyear/src/nine/February.java
@@ -0,0 +1,17 @@
+package nine;
+
+public class February {
+
+	public static void main(String[] args) {
+		int year = 2004;
+		
+		if((year %4==0)||(year%400==0)) {
+			System.out.println("leapyear");
+		}else {
+		System.out.println("not a leap year");
+		
+
+	}
+
+}
+}
